64996dc5ab9b7d9ab226e4f47dd9f8a4aeccb61b
yav-vas <yav.vasilev@gmail.com>
2021-12-02 23:01:32 +0200

    Implemented 3.2. - the edit function

--- Segment_tree/fast.cpp
+++ Segment_tree/fast.cpp
@@ -7,6 +7,7 @@ int arr[MAX_N + 2]; // as discussed in the book this can be omitted
 int st[2 * MAX_N + 2];
 
 void init(int);
+void edit(int, int, int);
 
 int main() {
 	int N;
@@ -33,7 +34,12 @@ int main() {
 
 		if (type == 'e') {
 			cin >> index >> val;
-			//insert edit function
+			edit(index, val, N);
+
+			//test if the edit function works
+			cout << "Segment tree:\n";
+			for (int i = 0; i < 2 * N; i++) cout << st[i] << " ";
+			cout << endl;
 		}
 
 		if (type == 's') {
@@ -49,4 +55,14 @@ void init(int N) {
 	for (int i = N; i < 2 * N; i++) st[i] = arr[i - N];
 
 	for (int i = N - 1; i > 0; i--) st[i] = st[2 * i] + st[2 * i + 1];
+}
+
+void edit(int pos, int val, int len) {
+	pos = len + pos; // update the first position to edit
+	int diff = val - st[pos];
+
+	while (pos != 0){ // exit the loop when 1 / 2 = 0 is reached
+		st[pos] += diff; // edit node
+		pos /= 2; // move up -> only O(logn) complexity
+	}
 }
