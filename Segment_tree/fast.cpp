304991028c9ff6e2e1e8fd0e2600e55a6dcd1272
yav-vas <yav.vasilev@gmail.com>
2021-12-05 21:01:02 +0200

    Implemented 3.3. - the sum function

--- Segment_tree/fast.cpp
+++ Segment_tree/fast.cpp
@@ -6,8 +6,13 @@ const int MAX_N = 1e6; // now that the solution is faster larger N is possible
 int arr[MAX_N + 2]; // as discussed in the book this can be omitted
 int st[2 * MAX_N + 2];
 
+void print(int);
+
 void init(int);
+
 void edit(int, int, int);
+
+int sum(int, int, int);
 
 int main() {
 	int N;
@@ -16,11 +21,6 @@ int main() {
 	for (int i = 0; i < N; i++) cin >> arr[i];
 
 	init(N);
-
-	//test if the init function works
-	cout << "Segment tree:\n";
-	for (int i = 0; i < 2 * N; i++) cout << st[i] << " ";
-	cout << endl;
 
 	int M;
 	cin >> M;
@@ -35,20 +35,22 @@ int main() {
 		if (type == 'e') {
 			cin >> index >> val;
 			edit(index, val, N);
-
-			//test if the edit function works
-			cout << "Segment tree:\n";
-			for (int i = 0; i < 2 * N; i++) cout << st[i] << " ";
-			cout << endl;
 		}
 
 		if (type == 's') {
 			cin >> start >> end;
 			//insert sum function
+			cout << sum(start, end, N) << endl;
 		}
 	}
 
 	return 0;
+}
+
+void print(int N) { // call it with N being the size of the array for debugging purposes only
+	cout << "Segment tree:\n";
+	for (int i = 0; i < 2 * N; i++) cout << st[i] << " ";
+	cout << endl;
 }
 
 void init(int N) {
@@ -63,6 +65,29 @@ void edit(int pos, int val, int len) {
 
 	while (pos != 0){ // exit the loop when 1 / 2 = 0 is reached
 		st[pos] += diff; // edit node
-		pos /= 2; // move up -> only O(logn) complexity
+		pos /= 2; // move up -> only O(logn) time complexity
 	}
+}
+
+int sum(int start, int end, int len) {
+	int l = len + start;
+	int r = len + end;
+	int sum = 0;
+
+	while (l != r) {
+		if (l % 2 == 1) {
+			sum += st[l];
+			l++;
+		}
+		if (r % 2 == 0) {
+			sum += st[r];
+			r--;
+		}
+		l /= 2; // move up -> only O(logn) time complexity
+		r /= 2; // move up -> only O(logn) time complexity
+	}
+
+	sum += st[l]; //does not matter whether l or r - they are equal
+	
+	return sum;
 }
