613e9a02974681e82f19d653c4ba7e60ea179d89
Iavor Vassilev <yav.vasilev@gmail.com>
2021-12-12 21:52:31 +0200

    Implemented 4.2. - The edit function
    Also made N be the size with the added zeros

--- Segment_tree/fast_recursive.cpp
+++ Segment_tree/fast_recursive.cpp
@@ -12,14 +12,34 @@ int realSize(int); // nearest to N bigger power of two
 
 void init(int, int);
 
+void edit(int, int, int, int, int, int);
+
 int main() {
 	int N;
 	cin >> N;
 
 	for (int i = 0; i < N; i++) cin >> arr[i];
 
-	init(1, realSize(N)); // start from root (index 1)
+	N = realSize(N); // change N to the real size at the beginning; needed for building the segment tree and everything else
 
+	init(1, N); // start from root (index 1)
+
+	int M;
+	cin >> M;
+
+	char type;
+	int index, val;
+	int start, end;
+
+	for (int i = 0; i < M; i++) {
+		cin >> type;
+
+		if (type == 'e') {
+			cin >> index >> val;
+			edit(index, val, N, 0, N - 1, 1);
+			print(N);
+		}
+	}
 	return 0;
 }
 
@@ -45,6 +65,20 @@ void init(int i, int N) { // here N is the real size given from the function
 
 	init(2 * i, N);
 	init(2 * i + 1, N);
+
+	st[i] = st[2 * i] + st[2 * i + 1];
+}
+
+void edit(int pos, int val, int N, int l, int r, int i) {
+	if (i >= N) {
+		st[i] = val;
+		return;
+	}
+
+	int mid = (l + r) / 2;
+	
+	if (pos <= mid) edit(pos, val, N, l, mid, 2 * i); // go to the left
+	else edit(pos, val, N, mid + 1, r, 2 * i + 1); // go to the right
 
 	st[i] = st[2 * i] + st[2 * i + 1];
 }
