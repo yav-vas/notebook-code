c7f7121c0f535abe261dfadd2622adc99d75fce2
Iavor Vassilev <yav.vasilev@gmail.com>
2022-02-06 13:52:07 +0200

    Revert "Iterative solution attempt removed and file structure change"
    This reverts commit ba61d89ac02bc7023dbded487d05150ce95b86b9.
    
    Found an iterative solution

---
+++ Segment_tree/Problems/p11235/p11235-iterative/p11235-iterative.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX_N = 1e5;
+
+struct node {
+	int left;
+	int right;
+	int freq;
+};
+
+node arr[MAX_N + 2];
+node st[2 * MAX_N + 2];
+
+int main() {
+	
+	return 0;
+}
